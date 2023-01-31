a33da2a5095f82e5441f224ae17b1401ce100ce9
shubhamsk591 <shubhamsinghk591@gmail.com>
2023-01-31 14:05:19 +0530

    Add ACRcloud
    Add ACRcloud API

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
-// Initialize variables
-let audioRecorder;
-let audioChunks = [];
-// Start recording function
+const recordButton = document.querySelector("#record-button");
+const stopButton = document.querySelector("#stop-button");
+const searchButton = document.querySelector("#search-button");
+const resultsContainer = document.querySelector("#results");
+
+let audioContext;
+let recorder;
+let audioBlob;
+let chunks = [];
+
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+searchButton.addEventListener("click", searchSong);
+
 function startRecording() {
-  // Set up the media recorder
-  navigator.mediaDevices.getUserMedia({audio: true})
-  .then(stream => {
-    audioRecorder = new MediaRecorder(stream);
-    audioRecorder.start();
-    audioRecorder.addEventListener("dataavailable", event => {
-      audioChunks.push(event.data);
+  recordButton.setAttribute("disabled", "disabled");
+  stopButton.removeAttribute("disabled");
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => {
+      audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      recorder = new MediaRecorder(stream);
+      recorder.start();
+
+      recorder.addEventListener("dataavailable", (event) => {
+        chunks.push(event.data);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  });
 }
 
-// Stop recording function
 function stopRecording() {
-  audioRecorder.stop();
-  audioRecorder.stream.getAudioTracks()[0].stop();
+  recordButton.removeAttribute("disabled");
+  stopButton.setAttribute("disabled", "disabled");
+  searchButton.removeAttribute("disabled");
 
-  // Create a new Blob from the audio chunks
-  let audioBlob = new Blob(audioChunks, {type: "audio/wav"});
+  recorder.stop();
 
-  // Create a new object URL for the audio
-  let audioUrl = URL.createObjectURL(audioBlob);
-
-  // Create a new audio element
-  let audioElement = new Audio(audioUrl);
-  audioElement.controls = true;
-  // Add the audio element to the page
-  document.getElementById("audio-player").appendChild(audioElement);
+  audioBlob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+  chunks = [];
 }
 
-// Save recording function
-function saveRecording() {
-  // Get the audio name from the input field
-  let audioName = document.getElementById("audio-name").value;
+async function searchSong() {
+  const API_KEY = "XjoaLaiRQERN6y1J";
+  const API_SECRET = "gR7zdpAVc9wdXCeNitjYGqscgUewD4tW";
 
-  // Create a new Blob from the audio chunks
-  let audioBlob = new Blob(audioChunks, {type: "audio/wav"});
+  const formData = new FormData();
+  formData.append("audio_data", audioBlob, "audio.ogg");
+  formData.append("access_key", API_KEY);
+  formData.append("timestamp", Date.now());
+  formData.append("signature", createSignature(API_SECRET, formData));
 
-  // Create a new object URL for the audio
-  let audioUrl = URL.createObjectURL(audioBlob);
+  const response = await fetch("https://api.acrcloud.com/v1/identify", {
+    method: "POST",
+    body: formData,
+  });
+  const json = await response.json();
 
-  // Create a link to download the audio
-  let downloadLink = document.createElement("a");
-  downloadLink.href = audioUrl;
-  downloadLink.download =audioName+".wav";
-  downloadLink.innerHTML = "Download " + audioName;
+  if (json.status.code === 0) {
+    const song = json.metadata.music[0];
+    resultsContainer.innerHTML = `
+      <h2>Song Information</h2>
+      <p>Title: ${song.title}</p>
+      <p>Artist: ${song.artists[0].name}</p>
+      <p>Album: ${song.album.name}</p>
+    `;
+  } else {
+    resultsContainer.innerHTML = "<p>No song found</p>";
+  }
+}
 
-  // Add the download link to the page
-  document.getElementById("audio-player").appendChild(downloadLink);
+function createSignature(apiSecret, formData) {
+  const message = Array.from(formData.entries())
+    .map((entry) => entry.join("="))
+    .join("&");
 
-  //reset the audio chunks
-  audioChunks = [];
+  return btoa(apiSecret + message);
 }
