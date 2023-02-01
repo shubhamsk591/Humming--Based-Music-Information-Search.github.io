581e1779c274c553df27e89da457e0a982a06004
shubhamsk591 <shubhamsinghk591@gmail.com>
2023-02-01 10:32:59 +0530

    test5

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-const recordButton = document.querySelector("#record-button");
-const stopButton = document.querySelector("#stop-button");
-const searchButton = document.querySelector("#search-button");
-const resultsContainer = document.querySelector("#results");
-
-let audioContext;
-let mediaRecorder;
-let audioBlob;
-let chunks = [];
-
-recordButton.addEventListener("click", startRecording);
-stopButton.addEventListener("click", stopRecording);
-searchButton.addEventListener("click", searchSong);
-
-async function startRecording() {
-  recordButton.setAttribute("disabled", "disabled");
-  stopButton.removeAttribute("disabled");
-
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  mediaRecorder = new MediaRecorder(stream);
-
-  mediaRecorder.start();
-  mediaRecorder.addEventListener("dataavailable", (event) => {
-    chunks.push(event.data);
+// Initialize variables
+let audioRecorder;
+let audioChunks = [];
+// Start recording function
+function startRecording() {
+  // Set up the media recorder
+  navigator.mediaDevices.getUserMedia({audio: true})
+  .then(stream => {
+    audioRecorder = new MediaRecorder(stream);
+    audioRecorder.start();
+    audioRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
   });
 }
 
+// Stop recording function
 function stopRecording() {
-  recordButton.removeAttribute("disabled");
-  stopButton.setAttribute("disabled", "disabled");
-  searchButton.removeAttribute("disabled");
-
-  mediaRecorder.stop();
-  audioBlob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-  chunks = [];
+  audioRecorder.stop();
+  audioRecorder.stream.getAudioTracks()[0].stop();
 }
-
-async function searchSong() {
+function searchRecording(){
   const API_KEY = "YXjoaLaiRQERN6y1J";
   const API_SECRET = "gR7zdpAVc9wdXCeNitjYGqscgUewD4tW";
 
   const formData = new FormData();
-  formData.append("audio_data", audioBlob, "audio.ogg");
+  formData.append("audio_data", audioRecorder, "audio/wav");
   formData.append("access_key", API_KEY);
   formData.append("timestamp", Date.now());
 
   const signature = btoa(API_KEY + ":" + API_SECRET);
 
-  const response = await fetch("https://api.acrcloud.com/v1/identify", {
+  const response =fetch("https://api.acrcloud.com/v1/identify", {
     method: "POST",
     headers: {
       "Content-Type": "multipart/form-data",
@@ -56,7 +39,37 @@ async function searchSong() {
     body: formData,
   });
 
-  const data = await response.json();
+  const data = response.json();
+  document.getElementById("results").appendChild(data);
 
-  // Handle response and display results in the "results" container
+}
+
+// Save recording function
+function saveRecording() {
+  // Get the song name from the input field
+  let songName = document.getElementById("song-name").value;
+
+  // Create a new Blob from the audio chunks
+  let audioBlob = new Blob(audioChunks, {type: "audio/wav"});
+
+  // Create a new object URL for the audio
+  let audioUrl = URL.createObjectURL(audioBlob);
+
+  // Create a new audio element
+  let audioElement = new Audio(audioUrl);
+
+  // Add the audio element to the page
+  document.getElementById("audio-player").appendChild(audioElement);
+
+  // Create a link to download the audio
+  let downloadLink = document.createElement("a");
+  downloadLink.href = audioUrl;
+  downloadLink.download = songName+".wav";
+  downloadLink.innerHTML = "Download " + songName;
+
+  // Add the download link to the page
+  document.getElementById("audio-player").appendChild(downloadLink);
+
+  //reset the audio chunks
+  audioChunks = [];
 }
